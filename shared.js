43a54d96febcd73ee8e8fd8dff14342fcab76b67
suryaprakash <surya.zvky@gmail.com>
2020-06-16 22:29:26 +0530

    added media queries to make the page responsive

--- shared.js
+++ shared.js
@@ -1,10 +1,46 @@
 var backdrop = document.querySelector('.backdrop')
-var buttons = document.querySelectorAll('.plan-list button')
+var selectedPlansButtons = document.querySelectorAll('.plan-list button')
 var modal = document.querySelector('.modal')
+var modalNoButton = document.querySelector('.modal__action--negative')
+var toggleButton = document.querySelector('.toggle-button')
+var mobileNavBar = document.querySelector('.mobile-nav')
 
-for(let i=0;i<buttons.length;i++){
-    buttons[i].addEventListener('click',function(){
-        modal.style.display='block';
+for(let i=0;i<selectedPlansButtons.length;i++){
+    selectedPlansButtons[i].addEventListener('click',function(){
+        if(modal){
+            modal.style.display='block';
+        }
         backdrop.style.display='block'
     })
+}
+
+toggleButton.addEventListener('click',function(){
+    mobileNavBar.style.display='block'
+    backdrop.style.display='block'
+})
+
+
+if(backdrop){
+    backdrop.addEventListener('click',function(){
+        mobileNavBar.style.display='none'
+      closeModal()
+    })
+}
+
+
+
+
+    modalNoButton.addEventListener('click',function(){
+        closeModal()
+    })
+
+
+
+function closeModal(){
+    backdrop.style.display='none'
+
+    if(modal){
+        modal.style.display='none'
+    }
+   
 }
